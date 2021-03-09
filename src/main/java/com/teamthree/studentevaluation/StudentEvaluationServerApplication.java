0f94071d410417e1c55f35bcee4522c2d05e69ee
Evaldas Grublys <evaldas.grublys@gmail.com>
2021-03-09 22:59:30 +0200

    Backend init

---
+++ src/main/java/com/teamthree/studentevaluation/StudentEvaluationServerApplication.java
@@ -0,0 +1,15 @@
+package com.teamthree.studentevaluation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages={"com.teamthree.studentevaluation"})
+public class StudentEvaluationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentEvaluationServerApplication.class, args);
+	}
+
+}
