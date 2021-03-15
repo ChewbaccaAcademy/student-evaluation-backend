edc40aad0b5ddb3eb6fee2252426dfb9e80443a3
Nikolaj Grigorjev <grigorjev.nikolaj@gmail.com>
2021-03-15 11:20:15 +0200

    secret key moved to app.prop

--- src/main/java/com/teamthree/studentevaluation/util/JwtUtil.java
+++ src/main/java/com/teamthree/studentevaluation/util/JwtUtil.java
@@ -3,6 +3,7 @@ package com.teamthree.studentevaluation.util;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +15,9 @@ import java.util.function.Function;
 
 @Service
 public class JwtUtil {
-    private String SECRET_KEY = "secret";
+
+    @Value("${secret.key}")
+    private String SECRET_KEY;
 
     public String extractUsername(String token){
         return extractClaim(token, Claims::getSubject);
@@ -42,7 +45,7 @@ public class JwtUtil {
 
     private String createToken(Map<String, Object> claims, String subject){
         return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10 ))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30 ))
                 .signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
     }
 
