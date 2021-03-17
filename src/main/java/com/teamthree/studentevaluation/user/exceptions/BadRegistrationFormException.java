280ed09e889e7596550e370e52cc758323563cb1
Evaldas Grublys <evaldas.grublys@gmail.com>
2021-03-17 18:36:05 +0200

    Typo for exception message register to registration

--- src/main/java/com/teamthree/studentevaluation/user/exceptions/BadRegistrationFormException.java
+++ src/main/java/com/teamthree/studentevaluation/user/exceptions/BadRegistrationFormException.java
@@ -3,7 +3,7 @@ package com.teamthree.studentevaluation.user.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Invalid register form.")
+@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Invalid registration form.")
 public class BadRegistrationFormException extends RuntimeException {
     public BadRegistrationFormException() {
         super();
