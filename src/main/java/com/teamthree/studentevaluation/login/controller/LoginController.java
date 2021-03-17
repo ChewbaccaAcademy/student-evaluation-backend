5f13dbe99f7555b277cad248e46a317dad8dc896
Gina Golubickiene <gina.gelzinyte@gmail.com>
2021-03-17 22:24:43 +0200

    added cross origin

--- src/main/java/com/teamthree/studentevaluation/login/controller/LoginController.java
+++ src/main/java/com/teamthree/studentevaluation/login/controller/LoginController.java
@@ -5,10 +5,7 @@ import com.teamthree.studentevaluation.login.models.AuthenticationRequest;
 import com.teamthree.studentevaluation.login.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class LoginController {
@@ -36,7 +33,8 @@ public class LoginController {
     }
 
     @PreAuthorize("permitAll")
-    @RequestMapping(path = "/authenticate", method = RequestMethod.POST)
+    @CrossOrigin
+    @RequestMapping(path = "/authenticate", method = RequestMethod.POST, produces="application/json")
     public String createAuthenticationToken(@RequestBody AuthenticationRequest
                                                     authenticationRequest) throws Exception {
         return loginService.authenticate(authenticationRequest);
