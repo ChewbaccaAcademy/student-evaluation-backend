ac6321b0a44400c907d74449fb25ba84cf42de9b
Evaldas Grublys <62142571+Pletkininkas@users.noreply.github.com>
2021-03-23 22:56:45 +0200

    Update IncorrectUserOrEmailException.java

--- src/main/java/com/teamthree/studentevaluation/login/exceptions/IncorrectUserOrEmailException.java
+++ src/main/java/com/teamthree/studentevaluation/login/exceptions/IncorrectUserOrEmailException.java
@@ -3,7 +3,7 @@ package com.teamthree.studentevaluation.login.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Bad username or email.")
+@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Bad email or password.")
 public class IncorrectUserOrEmailException extends RuntimeException {
     public IncorrectUserOrEmailException(String message) {
         super(message);
